ef06f7c6dc1f5047d845927d8e46afaedcbaa328
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2022-10-17 21:56:34 +0300

    lesson10

---
+++ lib/src/main/java/com/example/lib/Lesson10.java
@@ -0,0 +1,66 @@
+package com.example.lib;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Lesson10 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+
+        arrayList.add ("Show");
+        arrayList.add("must");
+        arrayList.add("go");
+
+        arrayList.remove("must");
+        System.out.println(arrayList.get(1));
+        arrayList.add(1, "hell");
+        System.out.println(arrayList.indexOf("go"));
+
+        LinkedList<String > linkedList = new LinkedList<>();
+        linkedList.add("we");
+
+        SortedSet<String> sortedSet = new TreeSet<>();
+        sortedSet.add("we");
+        sortedSet.add("are");
+        sortedSet.add("the");
+        sortedSet.add("champions");
+        sortedSet.add("champions");
+
+        System.out.println(sortedSet);
+
+
+        HashSet<Student> hashSet = new HashSet<>();
+        hashSet.add(new Student("Olya"));
+        hashSet.add(new Student("Olya"));
+        hashSet.add(new Student("Alex"));
+        hashSet.add(new Student("John"));
+        hashSet.add(new Student("Olya"));
+
+        System.out.println(hashSet.size());
+    }
+}
+
+class Student{
+    private String name;
+    Student(String name){
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
